f8de708435de41415aec9eb51c5d55ce8339626d
Alys <tiedie3000@hotmail.com>
2021-03-02 14:27:30 -0600

    solutions to problems

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,6 +1,8 @@
-const removeFromArray = function([...a],b) {
-    
-
+const removeFromArray = function(...args) {
+    //need more practice with arrays
+    const array = args[0];
+    return array.filter(val => !args.includes(val));
+   
 }
 
 module.exports = removeFromArray
--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0) {
+        return 'ERROR'
+    }
+    if (typeof a != 'number'|| typeof b != 'number') {
+        return 'ERROR'
+    }
+    let finalSum = 0;
+    let n = 0;
+    let c = Math.abs(b-a);
+    let it = (a+b) - 1;
+    for (i = 0; i < c; i++) {
+        n++;
+        finalSum += n;
+    }
+    return finalSum + it;
 }
+    
 
 module.exports = sumAll
