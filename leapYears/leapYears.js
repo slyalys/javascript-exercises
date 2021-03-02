9c57a9f31ffcd64f296d144db13865d0325fb01f
Alys <tiedie3000@hotmail.com>
2021-03-02 16:16:47 -0600

    Solutions

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,4 +1,16 @@
-const leapYears = function() {
+//divide by 4 first
+//if true check to see if divisible by 100
+//if divisible by 400 a leap year if not, not a leap year
+const leapYears = function(year) {
+    if(year%4 == 0) {
+        if(year%100 == 0 && year%400 == 0) {
+            return true;
+        }else if(year%100 == 0 && year%400 !=0) {
+            return false;
+        }
+       return true;
+    }
+    return false;
 
 }
 
